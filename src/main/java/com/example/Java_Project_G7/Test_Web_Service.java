29c8e640950e06a145140909a38369ba868f2e2d
FatemaSamir <201627220@std.sci.cu.edu.eg>
2022-03-09 20:22:55 +0200

    Skills_count

--- src/main/java/com/example/Java_Project_G7/Test_Web_Service.java
+++ src/main/java/com/example/Java_Project_G7/Test_Web_Service.java
@@ -1,7 +1,15 @@
 package com.example.Java_Project_G7;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class Test_Web_Service {
+    @GetMapping("/hello")
+    public String tes_fun(){
+        return "Hello, Welcome to our Project java";
+    }
+
+
 
 }
